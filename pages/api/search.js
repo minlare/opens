ec63eb809b9202ddfdf871a40243697b29a43b97
Michael Windell <michael@stormlightstudio.co.uk>
2023-03-26 17:12:18 +0100

    remove fs

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,22 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 import { build } from "search-params";
 
-function paramsToObject(entries) {
-  const result = {};
-  for (const [key, value] of entries) {
-    // each 'entry' is a [key, value] tupple
-    result[key] = value;
-  }
-  return result;
-}
+// function paramsToObject(entries) {
+//   const result = {};
+//   for (const [key, value] of entries) {
+//     // each 'entry' is a [key, value] tupple
+//     result[key] = value;
+//   }
+//   return result;
+// }
 
 const baseUrl = "https://global.sitesearch360.com/sites";
-const url = new URL(
-  "https://global.sitesearch360.com/sites?site=www.golfempire.co.uk&timeToAction=1679812127715&includeContent=true&limit=24&sort=Date&groupResults=false&highlightQueryTerms=true&filters=%5B%7B%22key%22%3A%22fid%235%22%2C%22name%22%3A%22type%22%2C%22values%22%3A%5B%7B%22name%22%3A%22Pairs%22%2C%22key%22%3A%22Pairs%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%234%22%2C%22name%22%3A%22gender%22%2C%22values%22%3A%5B%7B%22name%22%3A%22Male%22%2C%22key%22%3A%22Male%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%236%22%2C%22name%22%3A%22format%22%2C%22values%22%3A%5B%7B%22name%22%3A%224BBB%22%2C%22key%22%3A%224BBB%22%7D%2C%7B%22name%22%3A%22Combined%20Pairs%22%2C%22key%22%3A%22Combined%20Pairs%22%7D%2C%7B%22name%22%3A%22Foursomes%22%2C%22key%22%3A%22Foursomes%22%7D%2C%7B%22name%22%3A%22Greensomes%22%2C%22key%22%3A%22Greensomes%22%7D%2C%7B%22name%22%3A%222-Ball%20Texas%20Scramble%22%2C%22key%22%3A%222-Ball%20Texas%20Scramble%22%7D%2C%7B%22name%22%3A%22Other%22%2C%22key%22%3A%22Other%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%2312%22%2C%22name%22%3A%22age%22%2C%22values%22%3A%5B%7B%22name%22%3A%22Any%20Age%22%2C%22key%22%3A%22Any%20Age%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%2316%22%2C%22name%22%3A%22country%22%2C%22values%22%3A%5B%7B%22name%22%3A%22England%22%2C%22key%22%3A%22England%22%7D%5D%7D%5D&log=false&filterOptions=true&offset=48&query=*&plain=false&nameParsing=true&kvtable=false&complexFilterFormat=true"
-);
-const apiParams = paramsToObject(url.searchParams.entries());
+// const url = new URL(
+//   "https://global.sitesearch360.com/sites?site=www.golfempire.co.uk&timeToAction=1679812127715&includeContent=true&limit=24&sort=Date&groupResults=false&highlightQueryTerms=true&filters=%5B%7B%22key%22%3A%22fid%235%22%2C%22name%22%3A%22type%22%2C%22values%22%3A%5B%7B%22name%22%3A%22Pairs%22%2C%22key%22%3A%22Pairs%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%234%22%2C%22name%22%3A%22gender%22%2C%22values%22%3A%5B%7B%22name%22%3A%22Male%22%2C%22key%22%3A%22Male%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%236%22%2C%22name%22%3A%22format%22%2C%22values%22%3A%5B%7B%22name%22%3A%224BBB%22%2C%22key%22%3A%224BBB%22%7D%2C%7B%22name%22%3A%22Combined%20Pairs%22%2C%22key%22%3A%22Combined%20Pairs%22%7D%2C%7B%22name%22%3A%22Foursomes%22%2C%22key%22%3A%22Foursomes%22%7D%2C%7B%22name%22%3A%22Greensomes%22%2C%22key%22%3A%22Greensomes%22%7D%2C%7B%22name%22%3A%222-Ball%20Texas%20Scramble%22%2C%22key%22%3A%222-Ball%20Texas%20Scramble%22%7D%2C%7B%22name%22%3A%22Other%22%2C%22key%22%3A%22Other%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%2312%22%2C%22name%22%3A%22age%22%2C%22values%22%3A%5B%7B%22name%22%3A%22Any%20Age%22%2C%22key%22%3A%22Any%20Age%22%7D%5D%7D%2C%7B%22key%22%3A%22fid%2316%22%2C%22name%22%3A%22country%22%2C%22values%22%3A%5B%7B%22name%22%3A%22England%22%2C%22key%22%3A%22England%22%7D%5D%7D%5D&log=false&filterOptions=true&offset=48&query=*&plain=false&nameParsing=true&kvtable=false&complexFilterFormat=true"
+// );
+// const apiParams = paramsToObject(url.searchParams.entries());
 
 const options = {
   site: "www.golfempire.co.uk",
@@ -47,26 +47,24 @@ const fetchData = async (query, offset) => {
     res.json()
   );
 
-  const dataPath = results.activeFilterOptions
-    .map(
-      ({ name, values }) => `${name}-${values.map(({ key }) => key).join(":")}`
-    )
-    .join("/");
-
-  const saveDir = path.join(__dirname, "../../../../", "data", dataPath);
-  const saveFile = `data-${offset}.json`;
-  const saveFilePath = path.join(saveDir, saveFile);
-
-  if (!fs.existsSync(saveFilePath) && results.suggests?._?.length > 0) {
-    await fs.promises.mkdir(saveDir, {
-      recursive: true,
-    });
-    await fs.promises.writeFile(
-      path.join(saveDir, saveFile),
-      JSON.stringify(results),
-      "utf8"
-    );
-  }
+  // const dataPath = results.activeFilterOptions
+  //   .map(
+  //     ({ name, values }) => `${name}-${values.map(({ key }) => key).join(":")}`
+  //   )
+  //   .join("/");
+  // const saveDir = path.join(__dirname, "../../../../", "data", dataPath);
+  // const saveFile = `data-${offset}.json`;
+  // const saveFilePath = path.join(saveDir, saveFile);
+  // if (!fs.existsSync(saveFilePath) && results.suggests?._?.length > 0) {
+  //   await fs.promises.mkdir(saveDir, {
+  //     recursive: true,
+  //   });
+  //   await fs.promises.writeFile(
+  //     path.join(saveDir, saveFile),
+  //     JSON.stringify(results),
+  //     "utf8"
+  //   );
+  // }
 
   return results;
 };
@@ -84,15 +82,7 @@ export default async function handler(req, res) {
       offset = data.totalResults;
     }
     data = await fetchData(query, offset);
-    console.log(
-      "fetched",
-      offset,
-      data.suggests?._?.length,
-      data.totalResults,
-      returnData.length
-    );
     if (data.suggests?._?.length > 0) {
-      // console.log("adding", data.suggests._.length, offset);
       returnData.push(...data.suggests._);
     }
   }
